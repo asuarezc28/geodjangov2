9a2da833f7fd12f79a7e548dfbe8c93339e1396e
Ángel Suárez <asuarezc91@gmail.com>
2025-04-08 13:11:27 +0200

    Refactor Dockerfile to remove PostgreSQL and PostGIS dependencies
    - Eliminated PostgreSQL and PostGIS installation from the Dockerfile to streamline the build process and reduce image size.
    - Updated related documentation to reflect the changes in database management.

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install system dependencies
+apt-get update
+apt-get install -y \
+    gdal-bin \
+    libgdal-dev \
+    python3-gdal \
+    binutils \
+    libproj-dev \
+    libgeos-dev
+
+# Install Python dependencies
+pip install --upgrade pip
+pip install GDAL==$(gdal-config --version)
+pip install -r requirements.txt
+
+# Run Django commands
+python manage.py collectstatic --no-input
+python manage.py migrate 
