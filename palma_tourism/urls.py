be1bcb608fb2eb4ed7d6a67af4c479a4e4a64e46
Ángel Suárez <asuarezc91@gmail.com>
2025-04-08 15:34:43 +0200

    Update URL routing in urls.py to change API path
    - Modified the URL pattern to include the tourism app at the root path instead of the 'api/' prefix, simplifying access to the application's endpoints.

--- palma_tourism/urls.py
+++ palma_tourism/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('tourism.urls')),
+    path('', include('tourism.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
